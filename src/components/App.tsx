4a273b816f5212a8ddd1c0ccd3e9cbad352faf77
achman1729 <achin.madot@gmail.com>
2020-09-18 10:59:21 +1000

    modifed app

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,94 +1,90 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import AddMovie from '../pages/AddMovie'
-import Watchlist from '../pages/Watchlist';
-import useLocalStorage from '../hooks/localStorage'
-import moment from 'moment';
-import Axios from 'axios';
+import React, { useEffect } from 'react'
+import { Jumbotron, Container, Button } from 'reactstrap'
+// import AddMovie from './AddMovie'
+// import Watchlist from './Watchlist'
+import { useState } from 'react'
 
 interface Data {
-  rating:number, 
-  title:string, 
+  rating: number,
+  title: string,
   date: string,
-  apiData: [] 
+  apiData: any[]
 }
 
+const moviesArr: Data[] = []
+
+let avgRating: number = 0
+let totalRuntime: number = 0
+
+const stringToNumber = (runtime: string): number => {
+  let strNum: string[]
+  strNum = runtime.split(" ")
+  return parseInt(strNum[0])
+}
+
+localStorage.clear()
+
 const App: React.FC = () => {
-  // defining storage arrays for data
+  const [reload, setReload] = useState(false)
 
-  const [movie, setDateMovie] = useState({
-    rating:0,
-    title:"",
-    date:moment(new Date()).format('YYYY-MM-DD'),
-    apiData:[]
-  })
+  let avgRatingList = localStorage.getItem("AvgRating")
+  if (avgRatingList) {
+    avgRating = JSON.parse(avgRatingList)
+    }
 
-// request to omd API
-  const apiData = async (t: string) => {
-    let formatTitle = t.split(' ').join('+')
+  let totalRuntimeList = localStorage.getItem("TotalRuntime")
+  if (totalRuntimeList) {
+    totalRuntime = JSON.parse(totalRuntimeList)
+    }
 
-    await Axios.get(`http://www.omdbapi.com/?apikey=d37e246b&t=${formatTitle}`)
-    .then((response) =>{
-      console.log("this is from axios:", response.data)
-      // let genre = response.data.Genre
-      // let posterUrl = response.data.Poster
-      // console.log("api response data:", response)
-      // return response.data
-    })
-    .catch((error) =>{
-      console.log(error)
-    })
-  }
-
-  const handleTitle = (e: any) => {
-    console.log(e.target.value)
-    let title = e.target.value;
-
-    setDateMovie(prevState => {
-      return { ...prevState, title: title }
+  // getting data from AddMovie component
+  const getData = (data: Data): void => {
+    console.log("Data in app:", data)
+    moviesArr.push({
+      rating: data.rating,
+      title: data.title,
+      date: data.date,
+      apiData: data.apiData
     })
 
-  }
-  const handleDate = (e: any) => {
-    console.log(e.target.value)
+    setReload(true)
+    console.log("movies array: ", moviesArr)
+    
+    moviesArr.map((item: any) => {
+      if (moviesArr.length>0){
+      avgRating = (avgRating + item.rating) / moviesArr.length
+      totalRuntime +=stringToNumber(item.apiData.Runtime)
 
-    let date = e.target.value;
-
-    setDateMovie(prevState => {
-      return { ...prevState, date: date }
+      localStorage.setItem("AvgRating", JSON.stringify(avgRating))
+      localStorage.setItem("TotalRuntime", JSON.stringify(totalRuntime))
+      }
     })
-
-  }
-  const handleRating = (e: any) => {
-    console.log(e.target.value)
-
-    let rating = e.target.value;
-
-    setDateMovie(prevState => {
-      return { ...prevState, rating: rating }
-    })
-
+    console.log("average rating: ", avgRating)
   }
 
-  const handleSubmit = (e: React.SyntheticEvent) => {
-    apiData(movie.title)
-  }
+  useEffect(() => {
+    setReload(false)
+    localStorage.setItem("MovieList", JSON.stringify(moviesArr))
+  }, [reload])
 
   return (
     <>
-        {/* <Watchlist/> */}
-        <AddMovie 
-        handleTitle={handleTitle} 
-        handleDate={handleDate} 
-        handleRating={handleRating} 
-        handleSubmit={handleSubmit} 
-        rating={movie.rating}
-        title={movie.title}
-        date={movie.date}
-
-        />
+      <Jumbotron style={{ padding: "2rem 4rem" }}>
+        <h2>Movie Watchlist</h2>
+        <Container style={{ display: "flex", height: "1.2vh", justifyContent: "space-evenly", marginTop: "2rem" }}>
+          {avgRating === 0 ? 
+          <span><strong>Avg. rating: </strong> --- </span>
+          : 
+          <span><strong>Avg. rating: </strong>{avgRating.toFixed(1)} </span>
+        }
+          <span><strong>Total runtime watched:</strong> {totalRuntime} mins</span>
+        </Container>
+      </Jumbotron>
+      <AddMovie getData={getData} />
+      <hr />
+      <Watchlist />
     </>
-  );
-  }
+  )
+}
 
-export default App;
+export default App
